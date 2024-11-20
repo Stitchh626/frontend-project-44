6011d9283d96fe71246890c775494351a5eb1bf8
Alevtina <137526609+Stitchh626@users.noreply.github.com>
2024-11-21 01:34:51 +0200

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+import { greetUser } from './cli.js';  
+const roundsCount = 3;
+
+const runGame = (gameDescription, getQuestionAndAnswer) => {
+  const name = greetUser(); 
+  console.log(gameDescription);
+
+  for (let round = 0; round < roundsCount; round++) {
+    const { question, correctAnswer } = getQuestionAndAnswer();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default runGame;
