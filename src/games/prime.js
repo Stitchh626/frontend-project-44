5720b83eb65274787a37ac7cd166fc74f79b9be1
Alevtina <137526609+Stitchh626@users.noreply.github.com>
2024-11-21 03:13:31 +0200

    Add prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+// src/games/prime.js
+
+const isPrime = (num) => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+  
+  const getQuestionAndAnswer = () => {
+    const number = Math.floor(Math.random() * 100) + 1; // Генерируем случайное число от 1 до 100
+    const question = number.toString();
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  
+    return {
+      question,
+      correctAnswer,
+    };
+  };
+  
+  const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  
+  export { gameDescription, getQuestionAndAnswer };
+  
